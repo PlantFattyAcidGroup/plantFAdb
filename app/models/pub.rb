999b19087d86e9fd4c0961e2234ea4667d0664d2
Nicholas A. Thrower <throwern@msu.edu>
2015-11-11 09:50:28 -0500

    add paper_trail to pub

--- app/models/pub.rb
+++ app/models/pub.rb
@@ -2,6 +2,8 @@ class Pub < ActiveRecord::Base
   has_and_belongs_to_many :plants
   has_many :sofa_tabs
   has_many :results
+  has_paper_trail
+  
   def display_name
     "#{authors} #{year ? "(#{year})" : ''}. #{journal} #{volume} #{page}"
   end
