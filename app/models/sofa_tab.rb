65e07de3fd4045fccaa79f29a33a2cc49cdab341
Nicholas A. Thrower <throwern@msu.edu>
2016-06-24 17:00:44 -0400

    add sofa tab to results

--- app/models/sofa_tab.rb
+++ app/models/sofa_tab.rb
@@ -1,7 +1,10 @@
 class SofaTab < ActiveRecord::Base
   belongs_to :pub
   
+  def self.sofa_url(tab_id)
+    "http://sofa.mri.bund.de/Show_ResultDetail.php?Tab_ID="+(tab_id||'')
+  end
   def sofa_url
-    "http://sofa.mri.bund.de/Show_ResultDetail.php?Tab_ID="+(sofa_tab_id||'')
+    SofaTab.sofa_url(sofa_tab_id)
   end
 end
