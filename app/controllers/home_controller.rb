e859078c6d23bb6b3c72d15bf358f1ec82579db6
Nicholas A. Thrower <throwern@msu.edu>
2016-08-12 12:28:00 -0400

    open to public

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   skip_authorization_check
-  skip_before_action :authenticate_user!
+  #skip_before_action :authenticate_user!
   def index
     @page = Page.find_by(title: 'home')
   end
