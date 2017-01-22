e7c4fce9000e86f4e7e8eeee6aa40c8f21a88829
Nicholas A. Thrower <throwern@msu.edu>
2017-01-22 18:10:04 -0500

    skip draft dependents in Result

--- app/models/plants_pub.rb
+++ app/models/plants_pub.rb
@@ -5,17 +5,13 @@ class PlantsPub < ActiveRecord::Base
   validates :plant_id, presence: true, uniqueness: {scope: :pub_id}
   validates :pub_id, presence: true
   
+  has_paper_trail
   has_drafts
-  
   def display_name
     "#{plant.display_name} :: <i>#{pub.wos_title.truncate(40)}</i> #{pub.wos_authors.truncate(40)}".html_safe
   end
   
   def draft_publication_dependencies
-    results.map(&:draft).compact
-  end
-  
-  def draft_reversion_dependencies
     results.map(&:draft).compact
   end
   
--- config/initializers/draftsman.rb
+++ config/initializers/draftsman.rb
@@ -17,17 +17,13 @@ Draftsman.draft_class_name = 'Draft'
 # Override
 # Add dependencies function call to draft item
 class Draft < Draftsman::Draft
+  has_paper_trail
   def draft_publication_dependencies
+    #results are forcefully published with plants_pubs. Clear dependents to avoid looping
+    return [] if self.item.class==Result
     dependencies = super
     if defined? self.item.draft_publication_dependencies
       dependencies += self.item.draft_publication_dependencies
-    end
-    dependencies.compact.uniq
-  end
-  def draft_reversion_dependencies
-    dependencies = super
-    if defined? self.item.draft_reversion_dependencies
-      dependencies += self.item.draft_reversion_dependencies
     end
     dependencies.compact.uniq
   end
